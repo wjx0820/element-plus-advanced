6fdbba785a5aff0810d7a0de78a64a8d20198b1b
wjx0820 <1989wjx@gmail.com>
2021-12-17 12:07:53 +0800

    global register el-icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 
 <template>
   <router-view></router-view>
+  <br />
+  <el-button>I am ELButton</el-button>
+  <el-icon-edit />
 </template>
 
 <style lang='scss'>
 * {
   margin: 0;
   padding: 0;
+}
+svg {
+  width: 1em;
+  height: 1em;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import App from "./App.vue";
 import router from "./router/index";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as Icons from "@element-plus/icons-vue";
 
 const app = createApp(App);
+
+// 全局注册图标，牺牲一点性能
+// el-icon-xxx
+for (let i in Icons) {
+  // 注册全部组件
+  app.component(`el-icon-${i.toLowerCase()}`, (Icons as any)[i]);
+}
+
 app.use(router);
 app.use(ElementPlus);
 app.mount("#app");
