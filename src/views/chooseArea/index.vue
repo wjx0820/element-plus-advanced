2d17e9c7a461ca470e16ac7fe3d5f26a4fbbc73e
wjx0820 <1989wjx@gmail.com>
2021-12-19 15:50:50 +0800

    feat: chooseArea finish

---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <choose-area @change="changeArea"></choose-area>
+</template>
+
+ 
+<script lang='ts' setup>
+import ChooseArea from '@/components/chooseArea/src/index.vue'
+
+const changeArea = (data: any) => {
+  console.log(data)
+}
+</script>
+
+ 
+<style scoped>
+</style>
+
